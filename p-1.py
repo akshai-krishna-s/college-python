ea795648dfa619cc9d6af8410968ab71e3a4fd05
Akshai-Krishna-S <akshai404akz@gmail.com>
2023-07-12 16:08:19 +0530

    added changes

--- p-1.py
+++
@@ -1,6 +0,0 @@
-# Write a program to find the sum of two numbers
-
-n1 = int(input("Enter the first number: "))
-n2 = int(input("Enter the second number: "))
-
-print("The sum of the two numbers is: ", n1 + n2)
